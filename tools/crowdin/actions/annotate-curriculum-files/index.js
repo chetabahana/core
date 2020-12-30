b76eba48a9e710bf3c4a836b97ee62febe201615
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-12-30 12:06:44 +0100

    fix: invert regex and ignore dirs

--- tools/crowdin/actions/annotate-curriculum-files/index.js
+++ tools/crowdin/actions/annotate-curriculum-files/index.js
@@ -1,14 +1,14 @@
 const readDirP = require('readdirp');
 
-const learnRegex = /curriculum\/challenges\/english\/(?!.+part-\d\d\d\.md$)(?!\d\d-certificates).*/i;
+const ignoredFilesRE = /(part-\d\d\d\.md$)|(^\d\d-certificates)/i;
 const challengeDir = '../../../../curriculum/challenges/english';
 
-readDirP(challengeDir, {fileFilter: ['*.md'] }).on('data', file => {
-  if (file.stat.isFile()) {
-    console.log(file.path);
-    const match = file.path.match(learnRegex);
-    if (match) {
+readDirP(challengeDir, { fileFilter: ['*.md'], type: 'files' }).on(
+  'data',
+  file => {
+    const match = file.path.match(ignoredFilesRE);
+    if (!match) {
       console.log(file.path);
     }
   }
-});
+);
